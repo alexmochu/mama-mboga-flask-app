72dc93d0e58559c91611af737b104d29fd919cbb
Mochualex4 <Mochualex4@gmail.com>
2017-11-04 14:43:30 +0300

    Upadate __ini__.py
    Remove everything and add the following

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,5 @@
-# app/__init__.py
+from app.views import app
 
-# third-party imports
-from flask import Flask
 
-# local imports
-from config import app_config
-
-def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
-
+def create_app():
     return app
