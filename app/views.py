e27ce069d24c1eae835704bff204e353b3eb6395
Mochualex4 <Mochualex4@gmail.com>
2017-11-08 11:41:51 +0300

    Install flask-login which provides user session management
    Create a LoginManager object and initialize it

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 # app/views.py
 
 from flask import Flask, render_template
+from flask_login import LoginManager
 
 # local imports
 from config import app_config
@@ -8,6 +9,12 @@ from config import app_config
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(app_config['development'])
 app.config.from_pyfile('config.py')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_message = "You must be logged in to access this page."
+login_manager.login_view = "login"
+
     
 @app.route('/')
 def index():
