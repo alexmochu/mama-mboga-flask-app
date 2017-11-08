4bf5911b9931f9c95fdf82941071f54e121929fc
Mochualex4 <Mochualex4@gmail.com>
2017-11-08 15:14:00 +0300

    Fixed bugs and pylint errors

--- app/forms.py
+++ app/forms.py
@@ -2,12 +2,12 @@ from flask_wtf import Form
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, length, Email, EqualTo, ValidationError
 
-def validates_names(form, field):
+def validate_names(form, field):
     if field.data.isdigit():
-        raise ValidationError(u'Must Contain Letters)
+        raise ValidationError(u"Must Contain Letters")
 
 class SignUpForm(Form):
-    username = StringField("Username", validators=DataRequired(), length(min=4), validates_names)
-    email = StringField("Email", validators= [ DataRequired(), Email(message="Invalid Email Address"), length(min=4), validates_names])
-    password = PasswordField("New Password", validators=[DataRequired(), EqualTo('confirm'), message="Passwords must much"), length((min=4, max=80)])
+    username = StringField("Username", validators=[DataRequired(), length(min=4), validate_names ])
+    email = StringField("Email", validators= [ DataRequired(), Email(message="Invalid Email Address"), length(min=4), validate_names])
+    password = PasswordField('New Password', validators=[DataRequired(), EqualTo('confirm', message='Passwords must match'), length(min=4, max=80)])
     confirm = PasswordField("Confirm Password")
