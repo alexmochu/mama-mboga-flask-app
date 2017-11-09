96526a697b10331faea0b107e2130832d53a3d72
Mochualex4 <Mochualex4@gmail.com>
2017-11-09 12:05:39 +0300

    Add login form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, length, Email, EqualTo, ValidationError
 
 def validate_names(form, field):
@@ -11,3 +11,9 @@ class SignUpForm(Form):
     email = StringField("Email", validators= [ DataRequired(), Email(message="Invalid Email Address"), length(min=4), validate_names])
     password = PasswordField('New Password', validators=[DataRequired(), EqualTo('confirm', message='Passwords must match'), length(min=4, max=80)])
     confirm = PasswordField("Confirm Password")
+    submit = SubmitField('Register')
+
+class LoginForm(Form):
+    email = StringField("Email", validators=[DataRequired(), length(min=4), validate_names])
+    password = PasswordField('Password', validators=[DataRequired(), length(min=4, max=80)])
+    submit = SubmitField('Login')
